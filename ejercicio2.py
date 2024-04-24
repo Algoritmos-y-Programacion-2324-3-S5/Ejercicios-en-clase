ac324863c202ea98462a136528e636ea5dfae54b
aguerraUnimet <98197577+aguerraUnimet@users.noreply.github.com>
2024-04-24 10:27:13 -0400

    Semana 1

---
+++ ejercicio2.py
@@ -0,0 +1,4 @@
+base = input("Please enter the base: \n->")
+height = input("Please enter the height: \n->")
+area = int(base) * int(height) / 2
+print(f"The area is: {area}")
---
+++ ejercicio3.py
@@ -0,0 +1,3 @@
+number = int(input("Please enter the number: \n->"))
+result = number * (number + 1) / 2
+print(f"The summatory is: {result}")
